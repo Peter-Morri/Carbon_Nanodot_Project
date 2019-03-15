18e4a6e57d63b9767db656bf79fdb3aaa9fbf956
Peter Morrison <P.Morrison@warwick.ac.uk>
2019-03-15 13:31:32 +0000

    Added new script to query data path and pass it to our new function Dataframes. DF now print(s) progress to console.

--- Dataframes.R
+++ Dataframes.R
@@ -1,49 +1,35 @@
-## Install Software -------------------------------
-# source("http://bioconductor.org/biocLite.R")
-# biocLite("flowCore")
-##
-
-## Initialise R: ##
-
-## Load Libraries ---------------------------------
-library(flowCore)
-library(tidyverse)
-library(mclust) 
-
-# Set the working directory ----------------------
-#setwd("~/Documents/Data/Scripts/Intelligate")
-
-
+dataFrames <- function(dataPath){
+  
 ## Import and pre-process data: ##
+# Load FlowFrames from path into FlowSet:
+fs <- read.flowSet(path = dataPath, pattern = ".fcs", alter.names = TRUE, phenoData = list(name = "SAMPLE ID", Filename = "$FIL"))
 
-# Load FlowFrames from file into FlowSet ---------
-fs <- read.flowSet(path = 'Data/18-03-29_PulseChase2-roots', pattern = ".fcs", alter.names = TRUE, phenoData = list(name = "SAMPLE ID", Filename = "$FIL"))
-
-# Create Raw DataFrames --------------------------
-
+##  Create Raw DataFrames
 # create empty list to hold each DataFrame 
 dflist <- list(length(fs))
 
-# loop over the FlowSet and assign a DataFrame containing the expression values to each position in the list
+print("Importing files...")
 for (i in 1:(length(fs))) {
   dflist[[i]] <- data.frame(as.data.frame(exprs(fs[[i]])))
   dflist[[i]] <- na.omit(dflist[[i]]) # remove NA values
   for (j in 1:length(fs@colnames)) {dflist[[i]] <- dflist[[i]][dflist[[i]][,j] !="-Inf",]} # remove any "-Inf" values
 }
 
-# Create Log-transformed DataFrames --------------------------
-
-# log-transform all channels (except time in column 1)
+print("Creating Log-transforms...")
+# log-transform all channels (except time i.e. column 1)
 fs_log <- transform(fs, transformList(fs@colnames[2:16], log)) 
 
 # create empty list to hold each DataFrame 
 dflist_log <- list(length(fs_log))
 
-# loop over the FlowSet and assign a DataFrame containing the log-transformed expression values to each position in the list
+print("Populating DataFrames...")
 for (i in 1:(length(fs_log))) {
   dflist_log[[i]] <- data.frame(as.data.frame(exprs(fs_log[[i]])))
   dflist_log[[i]] <- na.omit(dflist_log[[i]]) # remove NA values
   for (j in 1:length(fs_log@colnames)) {dflist_log[[i]] <- dflist_log[[i]][dflist_log[[i]][,j] !="-Inf",] # removing the negative values that gave "-Inf" after log-transforming 
   dflist_log[[i]] <- dflist_log[[i]][dflist_log[[i]][,j] > 1,]} # remove outliers by only keeping expression values greater than 1
 } 
+
+return(dflist_log)
+}
 
---
+++ Intelligate.R
@@ -0,0 +1,35 @@
+# Install Software:
+# source("http://bioconductor.org/biocLite.R")
+# biocLite("flowCore")
+
+
+### Initialise R: ###
+
+
+## Load Libraries:
+library(flowCore)
+library(tidyverse)
+library(mclust) 
+## Set the working directory:
+# setwd("~/Documents/Data/Scripts/Intelligate")
+## Include Intelligate core functions:
+source("DataFrames.R")
+#source("mclust_dots.R")
+#source("mclust_roots.R")
+#source("mclust_leaves.R")
+#source("Figure_plotting.R")
+
+### Start workflow: ###
+
+## Query path to FCS data:
+liPrompt <- readline( prompt = "Enter name of folder containing data for analysis: Data/")
+fcPath = paste("Data/",liPrompt, sep = "")
+print("OK! Working: ")
+logExprs <- dataFrames(fcPath)
+print("Complete.")
+
+liPrompt <- userQuery("Begin mclust? ", default = "n")
+
+if(liPrompt == "y"){
+  print("OK, clustering:")
+}
